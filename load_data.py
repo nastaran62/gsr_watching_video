Add baseline normalization, fixing bug in kfold and change labeling threshold

load_data.py
@@ -83,15 +83,15 @@ def load_all_labels(path):
             all_participants[participant_number]["emotion"].append(4)
 
         all_participants[participant_number]["intensity"].append(row["intensity"])
-        if row["valence"] < 4:
-            all_participants[participant_number]["valence"].append(0)
-        elif row["valence"] >= 4:
+        if row["valence"] >= 5:
             all_participants[participant_number]["valence"].append(1)
-        if row["arousal"] > 4:
+        else:
+            all_participants[participant_number]["valence"].append(0)
+        if row["arousal"] > 5:
             all_participants[participant_number]["arousal"].append(1)
         else:
             all_participants[participant_number]["arousal"].append(0)
-        if row["dominance"] >= 4:
+        if row["dominance"] > 5:
             all_participants[participant_number]["dominance"].append(1)
         else:
             all_participants[participant_number]["dominance"].append(0)
@@ -104,13 +104,13 @@ def load_all_physiological(path, all_participants):
         all_trials_data = []
         trials_path = os.path.join(path,
                                    "p{0}".format(str(participant_number).zfill(2)),
-                                   "physiological_csv")
+                                   "gsr")
         all_trials = os.listdir(trials_path)
         all_trials.sort()
         for trial in all_trials:
             trial_path = os.path.join(trials_path, trial)
             trial_data = np.loadtxt(trial_path, delimiter=',')
-            all_trials_data.append(trial_data)
+            all_trials_data.append(trial_data[-10624:])
         all_physiologcal.append(np.array(all_trials_data))
     print(np.array(all_physiologcal).shape)
     return np.array(all_physiologcal)

main.py
@@ -6,10 +6,9 @@ from cnn_lstm_classification import cnn_lstm_classification
 from simple_classification import feature_classification, kfold_testing
 
 
-PART_SECONDS = 5
+WINDOW_SIZE = 1
 LABEL_TYPE = "arousal"
 GSR_SAMPLING_RATE = 128
-IGNORE_TIME = 8
 
 if LABEL_TYPE == "emotion":
     CLASSES = [0, 1, 2, 3, 4]
@@ -18,7 +17,7 @@ else:
 
 
 def prepare_experimental_data():
-    path = "data/prepared_data"
+    path = "data/prepared_data0"
     participant_list = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
                         32, 33, 34, 35, 36, 37, 38, 40, 41, 42, 43]
     label_path = "data/labels/self_report.csv"
@@ -26,9 +25,6 @@ def prepare_experimental_data():
     all_labels = \
         load_labels(labels, participant_list, type=LABEL_TYPE)
     labels = all_labels
-    # CLASSES COUNT
-    for i in range(len(CLASSES)):
-        print("class count", CLASSES[i], (np.array(all_labels) == CLASSES[i]).sum())
 
     physiological_data = load_all_physiological(path, participant_list)
 
@@ -39,7 +35,7 @@ def prepare_experimental_data():
         for t in range(trials):
             # preprocessing
             # Ignores 8 seconds from the start of each trial
-            data = physiological_data[p, t, IGNORE_TIME*GSR_SAMPLING_RATE:, 1]
+            data = physiological_data[p, t, :]
             preprocessed_physiological = \
                 physiological_preprocessing(data,
                                             sampling_rate=GSR_SAMPLING_RATE)
@@ -67,7 +63,7 @@ def prepare_deap_data():
         for t in range(gsr_data.shape[1]):
             # preprocessing
             # Ignores IGNORE_TIME seconds from the start of each trial
-            data = gsr_data[p, t, 0, IGNORE_TIME*GSR_SAMPLING_RATE:]
+            data = gsr_data[p, t, 0, :]
             preprocessed_physiological = \
                 physiological_preprocessing(data,
                                             sampling_rate=GSR_SAMPLING_RATE)
@@ -81,15 +77,15 @@ def prepare_deap_data():
 
 
 # Loading deap dataset
-physiological_data, labels = prepare_deap_data()
+#physiological_data, labels = prepare_deap_data()
 
 # Loading experimental dataset
-#physiological_data, labels = prepare_experimental_data()
+physiological_data, labels = prepare_experimental_data()
 
-# lstm_classification(physiological_data, labels, PART_SECONDS,
+# lstm_classification(physiological_data, labels, WINDOW_SIZE,
 #                    CLASSES, sampling_rate=GSR_SAMPLING_RATE)
 #cnn_lstm_classification(physiological_data, labels, CLASSES)
-# feature_classification(physiological_data, labels, PART_SECONDS,
+# feature_classification(physiological_data, labels, WINDOW_SIZE,
 #                       CLASSES, sampling_rate=GSR_SAMPLING_RATE)
-kfold_testing(physiological_data, labels, PART_SECONDS,
+kfold_testing(physiological_data, labels, WINDOW_SIZE,
               CLASSES, sampling_rate=GSR_SAMPLING_RATE)

physiological/feature_extraction.py
@@ -226,24 +226,15 @@ def minimum(data):
 
 
 def get_gsr_features(gsr_data):
-    #phasic, tonic = extract_gsr_components(data[i, 0, :], 128)
-    #phasic_features = [np.mean(phasic), np.std(phasic)]
-    #tonic_features = [np.mean(tonic), np.std(tonic)]
-    # gsr_features = [np.mean(gsr_data), np.std(gsr_data)]
-    # diff = np.diff(gsr_data, n=1)
-    # diff2 = np.diff(gsr_data, n=2)
-    # diff_features = [np.mean(diff), np.std(diff)]
-    # diff_features2 = [np.mean(diff2), np.std(diff2)]
-    # d1 = prop_neg_derivatives(diff)
-    # d2 = prop_neg_derivatives(diff2)
-
-    # feature = \
-    #     gsr_features  # + diff_features + diff_features2 + d1 + d2
-    # # _get_frequency_features(gsr_data)
-    # # [gsr_entropy]
-    gsr_features = mean(gsr_data) + \
-        standard_deviation(gsr_data) + get_frequency_peak(gsr_data) + \
-        get_max_amp_peak(gsr_data) + sum_of_negative_derivative(gsr_data)
+    gsr_features = mean(gsr_data) + median(gsr_data) + \
+        get_frequency_peak(gsr_data) + \
+        get_var_amp_peak(gsr_data) + \
+        sum_of_positive_derivative(gsr_data) + \
+        sum_of_negative_derivative(gsr_data) + \
+        std_amp_peak(gsr_data) + \
+        skewness_amp_peak(gsr_data) + \
+        kurtosis_amp_peak(gsr_data) + max_abs_amp_peak(gsr_data) + \
+        variance(gsr_data) + standard_deviation(gsr_data)
     return np.array(gsr_features)
 
 

physiological/preprocessing.py
@@ -23,8 +23,10 @@ def physiological_preprocessing(physiological_data, sampling_rate=128):
                                              sampling_rate)
     #data = normalization(np.array(preprocessed_gsr))
     # display_signal(normalization(np.array(preprocessed_gsr)))
-
-    return preprocessed_gsr
+    normalized = baseline_normalization(preprocessed_gsr[sampling_rate*3:],
+                                        preprocessed_gsr[0:3*sampling_rate],
+                                        128)
+    return normalized
 
 
 def gsr_noise_cancelation(data, sampling_rate, low_pass=0.1, high_pass=15):
@@ -36,13 +38,18 @@ def gsr_noise_cancelation(data, sampling_rate, low_pass=0.1, high_pass=15):
     output = signal.filtfilt(b, a, np.array(data, dtype=np.float))
 
     # Removing rapid transient artifacts
-    final_output = signal.medfilt(output, kernel_size=5)
-    return final_output
-
-
-def normalization(data):
-    # Normalization
-    min = np.amin(data)
-    max = np.amax(data)
-    output = (data - min) / (max - min)
+    output = signal.medfilt(output, kernel_size=5)
     return output
+
+
+def baseline_normalization(data, baseline, sampling_rate=128):
+    length = int(baseline.shape[0] / sampling_rate)
+    all = []
+    for i in range(length):
+        all.append(baseline[i*sampling_rate:(i+1)*sampling_rate])
+    baseline = np.mean(np.array(all), axis=0)
+
+    window_count = round(data.shape[0] / sampling_rate)
+    for i in range(window_count):
+        data[i*sampling_rate:(i+1)*sampling_rate] -= baseline
+    return data

simple_classification.py
@@ -81,9 +81,9 @@ def feature_classification(physiological_data, labels, part_seconds, classes, sa
     physiological_data = np.array(all_physiological_features)
     all_participants_labels = np.array(all_participants_labels)
 
-    physiological_train, physiological_test, \
-        y_train, y_test = \
-        leave_one_subject_out_split(physiological_data, all_participants_labels)
+    #physiological_train, physiological_test, \
+    #    y_train, y_test = \
+    #    leave_one_subject_out_split(physiological_data, all_participants_labels)
 
     physiological_train, physiological_test, \
         y_train, y_test = \
@@ -101,23 +101,14 @@ def feature_classification(physiological_data, labels, part_seconds, classes, sa
 
 
 def physiological_classification(x_train, x_test, y_train, y_test, classes):
-    # clf = svm.SVC(probability=True)
-    #clf = svm.SVC(C=150, kernel="rbf", gamma="auto", probability=True, class_weight='balanced')
-    #clf = KNeighborsClassifier(n_neighbors=len(classes)+1)
-    clf = RandomForestClassifier(n_estimators=200, class_weight='balanced')
-    # clf = AdaBoostClassifier(n_estimators=100, learning_rate=1)
-    #clf = GaussianNB()
-    # clf = QuadraticDiscriminantAnalysis()
-    # clf = LinearDiscriminantAnalysis()
-    # clf = MLPClassifier()
-    # clf = LinearDiscriminantAnalysis()
-    clf.fit(x_train, y_train)
-    print("physiological prediction", clf.predict(x_test))
-    pred_values = clf.predict_proba(x_test)
+    train_mean = np.mean(x_train)
+    train_std = np.std(x_train)
+    x_train = (x_train - train_mean) / train_std
+    x_test = (x_test - train_mean) / train_std
 
-    # acc = accuracy_score(pred_values, y_test)
-    # print(classification_report(y_test, pred_values))
-    # print(acc)
+    clf = RandomForestClassifier(n_estimators=200, max_features="auto", class_weight='balanced')
+    clf.fit(x_train, y_train)
+    pred_values = clf.predict_proba(x_test)
     return pred_values
 
 
@@ -135,16 +126,9 @@ def normal_train_test_split(physiological_data, labels):
     physiological_train, physiological_test, y_train, y_test = \
         train_test_split(np.array(physiological_features),
                          np.array(labels),
-                         test_size=0.2,
+                         test_size=0.3,
                          random_state=100,
                          stratify=labels)
-
-    scaler = MinMaxScaler()
-    # Fit on training set only.
-    # Apply transform to both the training set and the test set.
-    scaler.fit(physiological_train)
-    physiological_train = scaler.transform(physiological_train)
-    physiological_test = scaler.transform(physiological_test)
     return physiological_train, physiological_test, \
         y_train, y_test
 
@@ -257,6 +241,7 @@ def kfold_testing(physiological_data, labels, part_seconds, classes, sampling_ra
     else:
         part_length = part_seconds * sampling_rate
         part_count = int(points / part_length)
+        print(part_count)
     all_participants_labels = []
     for p in range(participants):
         all_trials_physiological = []
@@ -278,8 +263,11 @@ def kfold_testing(physiological_data, labels, part_seconds, classes, sampling_ra
     physiological_features = \
         physiological_data.reshape(-1, physiological_data.shape[-1])
     labels = \
-        np.array(labels).reshape(-1)
-
+        np.array(all_participants_labels).reshape(-1)
+    # CLASSES COUNT
+    CLASSES = [0, 1]
+    for i in range(len(CLASSES)):
+        print("class count", CLASSES[i], (np.array(labels) == CLASSES[i]).sum())
     k = 5
     kf = KFold(n_splits=k, shuffle=True, random_state=100)
     sum_fscore = 0
@@ -288,13 +276,15 @@ def kfold_testing(physiological_data, labels, part_seconds, classes, sampling_ra
     for train_index, test_index in kf.split(labels):
         physiological_train, physiological_test = \
             physiological_features[train_index, :], physiological_features[test_index, :]
-
-        mean = np.mean(physiological_train)
-        std = np.std(physiological_train)
-        physiological_train = (physiological_train - mean) / std
-        physiological_test = (physiological_test - mean) / std
+        #mean = np.mean(physiological_train)
+        #std = np.std(physiological_train)
+        #physiological_train = (physiological_train - mean) / std
+        #physiological_test = (physiological_test - mean) / std
 
         y_train, y_test = labels[train_index], labels[test_index]
+        print(physiological_train.shape, physiological_test.shape)
+        print(y_train.shape, y_test.shape)
+        input()
         preds_physiological = \
             physiological_classification(
                 physiological_train, physiological_test, y_train, y_test, classes)